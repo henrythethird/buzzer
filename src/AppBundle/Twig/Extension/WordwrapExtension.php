3dcdf65d3ca514fba0846df25cd42f622dc55d3e
Patrick Kaufmann <patrick.kaufmann@pwc-digital.ch>
2017-03-02 12:49:01 +0100

    Added repeat to twig extensions

--- src/AppBundle/Twig/Extension/WordwrapExtension.php
+++ src/AppBundle/Twig/Extension/WordwrapExtension.php
@@ -7,13 +7,29 @@ class WordwrapExtension extends \Twig_Extension
     public function getFilters()
     {
         return [
-            new \Twig_SimpleFilter('wordwrap', [$this, 'wordwrap'])
+            new \Twig_SimpleFilter('wordwrap', [$this, 'wordwrap']),
+            new \Twig_SimpleFilter('repeat', [$this, 'repeat']),
         ];
     }
 
+    /**
+     * @param string $text
+     * @param int $charCount
+     * @return string
+     */
     public function wordwrap($text, $charCount = 42)
     {
         return wordwrap($text, $charCount);
+    }
+
+    /**
+     * @param string $text
+     * @param int $multiplier
+     * @return string
+     */
+    public function repeat($text, $multiplier)
+    {
+        return str_repeat($text, $multiplier);
     }
 
     /**
